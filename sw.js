2a21460de06334f333aa088ed15b756351e5d777
orenoadores0224-tech <orenoadores0224@gmail.com>
2025-12-30 15:45:17 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,29 +1,43 @@
-const CACHE_NAME = "kofu-pwa-v1";
+const CACHE_NAME = 'kofu-pwa-v1';
+
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./shouba.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  './',
+  './index.html',
+  './shouba.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+      Promise.all(keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : null)))
     )
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((res) => {
+        // 同一オリジンだけキャッシュ
+        const url = new URL(event.request.url);
+        if (url.origin === location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return res;
+      }).catch(() => cached);
+    })
   );
 });
