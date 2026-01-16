f7171fea8d11b10597332af6d59997832d1e7477
orenoadores0224-tech <orenoadores0224@gmail.com>
2026-01-16 18:37:02 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -3,13 +3,19 @@ const CACHE_NAME = 'kofu-pwa-v2';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
+  './sw.js',
+
+  './icon-192.png',
+  './icon-512.png',
+
   './shouba.html',
   './kotu.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './coldshower.html',
+  './ba-kansoku.html'
 ];
 
+// インストール時にキャッシュ
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
@@ -17,6 +23,7 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
+// 古いキャッシュ削除
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -26,8 +33,21 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
+// 基本はキャッシュ優先（オフライン強い）
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((res) => {
+        // 同一オリジンのGETだけキャッシュ
+        try {
+          const url = new URL(event.request.url);
+          if (event.request.method === 'GET' && url.origin === self.location.origin) {
+            const copy = res.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          }
+        } catch (_) {}
+        return res;
+      }).catch(() => cached);
+    })
   );
 });
