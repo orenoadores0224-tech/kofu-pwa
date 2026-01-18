c136b0fbaf18f9f833d0746aaada42163c4e8072
orenoadores0224-tech <orenoadores0224@gmail.com>
2026-01-19 07:48:21 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+// sw.js
 const CACHE_NAME = "kofu-cache-v7";
 
 const PRECACHE_URLS = [
@@ -15,3 +16,27 @@ const PRECACHE_URLS = [
   "./IMG_3564.jpeg",
   "./IMG_3565.jpeg"
 ];
+
+// install
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+// activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
